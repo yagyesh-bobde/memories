d6fc07f9afd184a64842d63b8ff96a21e5e90114
yagyesh_bobde <yagyeshsrbobde@gmail.com>
2024-08-07 12:24:59 +0530

    updated the ui dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { Component } from "@/components/component";
 
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Memories
+    <main className="">
+      <Component />
     </main>
   );
 }
